430b136e7bd12b639670680d2f668091f4a95fca
Shevlyakov Vladislav <vladislavs319@gmail.com>
2019-11-17 01:11:24 +0200

    Test WebSocket!

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,6 +18,7 @@ type templateBuilder struct {
 func main() {
 	fmt.Println("Hello chat")
 	http.HandleFunc("/", root)
+	http.HandleFunc("/chat", chatHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -28,8 +30,30 @@ func root(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-
 	if err := temple.Execute(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func chatHandler(w http.ResponseWriter, r *http.Request) {
+	conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+	for {
+		msgType, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("%s user say: %s\n", conn.RemoteAddr(), string(msg))
+
+		if err = conn.WriteMessage(msgType, msg); err != nil {
+			break
+		}
+	}
+
 }
