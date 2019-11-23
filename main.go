dd82c9d8b9ccf02c3009631a04b82c42222a6b08
Shevlyakov Vladislav <vladislavs319@gmail.com>
2019-11-23 16:16:53 +0200

    setup of React

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
 	"html/template"
 	"log"
 	"net/http"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
 
 type templateBuilder struct {
