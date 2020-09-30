ee1d98a3ba3ef06b67b9df28ca1c6be4eac0ba70
Shevlyakov Vladislav <vladislavs319@gmail.com>
2020-09-30 20:56:47 +0300

    fetch products by by local ids

--- src/pages/Cart/CartItem/CartItem.tsx
+++ src/pages/Cart/CartItem/CartItem.tsx
@@ -2,18 +2,29 @@ import React from 'react'
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core'
 
-interface CartItemProps {}
+export type CartItemType = {
+  title: string
+  price: number
+  amount: number
+  preview: string
+}
+
+interface CartItemProps {
+  item: CartItemType
+}
 
 const useStyles = makeStyles(() => ({
   root: {}
 }))
 
-const CartItem: React.FC<CartItemProps> = () => {
+const CartItem: React.FC<CartItemProps> = ({ item }) => {
   const classes = useStyles()
 
   return (
     <Box>
-      <p>Cart Item</p>
+      <p>{item.title}</p>
+      <p>price: {item.price}</p>
+      <p>amount: {item.amount}</p>
     </Box>
   )
 }
--- src/pages/Cart/CartItems/CartItems.tsx
+++ src/pages/Cart/CartItems/CartItems.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Box from '@material-ui/core/Box'
 import Grid from '@material-ui/core/Grid'
-import CartItem from '../CartItem/CartItem'
+import CartItem, { CartItemType } from '../CartItem/CartItem'
 import { makeStyles } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import { useQuery, useMutation } from '@apollo/react-hooks'
@@ -13,7 +13,7 @@ interface CartTotals {
   cartItems: string[]
 }
 
-const GET_CART_TOTALS = gql`
+const GET_CART_IDS = gql`
   {
     cartItems @client
   }
@@ -25,6 +25,17 @@ const CLEAR_CART = gql`
   }
 `
 
+const GET_ALL_PRODUCTS_BY_ID = gql`
+  query productsByID($ids: [String!]) {
+    productsByID(ids: $ids) {
+      title
+      price
+      amount
+      preview
+    }
+  }
+`
+
 const useStyles = makeStyles(() => ({
   root: {},
   list: {}
@@ -32,7 +43,16 @@ const useStyles = makeStyles(() => ({
 
 const CartItems: React.FC<CartItemsProps> = () => {
   const classes = useStyles()
-  const { data } = useQuery<CartTotals>(GET_CART_TOTALS)
+  const savedIDS = useQuery<CartTotals>(GET_CART_IDS)
+  const { data, loading } = useQuery(GET_ALL_PRODUCTS_BY_ID, {
+    variables: {
+      ids: savedIDS.data?.cartItems
+    },
+    fetchPolicy: 'network-only'
+  })
+
+  console.log(data)
+
   const [onClearCart] = useMutation(CLEAR_CART)
 
   const handleClearAllClick = (): void => {
@@ -45,13 +65,17 @@ const CartItems: React.FC<CartItemsProps> = () => {
       {/* temp solution  */}
       <Button onClick={handleClearAllClick}>Clear all</Button>
       {/*  */}
-      <Grid container component="ul" className={classes.list}>
-        {data?.cartItems.map((product, ind) => (
-          <Grid key={ind} component="li" item xs={12} md={4} lg={3} xl={2}>
-            <CartItem />
-          </Grid>
-        ))}
-      </Grid>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <Grid container component="ul" className={classes.list}>
+          {data?.productsByID.map((product: CartItemType, ind: number) => (
+            <Grid key={ind} component="li" item xs={12}>
+              <CartItem item={product} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
     </Box>
   )
 }
