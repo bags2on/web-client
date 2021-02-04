3e4f346c7b54be924ed51c29a297167ee6d557b2
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-02-04 12:50:55 +0200

    remove jupper / media section added / sub footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import NightToggleSwith from '../../shared/NightToggleSwith/NightToggleSwith'
+// import NightToggleSwith from '../../shared/NightToggleSwith/NightToggleSwith' // TODO
 import Typography from '@material-ui/core/Typography'
-import Japer from './Japer/Japer'
 import Icon from '@material-ui/core/Icon'
 import { Link } from 'react-router-dom'
 import { ReactComponent as PhoneIcon } from '../../assets/svg/phone-call.svg'
-
+import { ReactComponent as InstagramIcon } from '../../assets/svg/instagram.svg'
+import { ReactComponent as TelegramIcon } from '../../assets/svg/telegram.svg'
+import { ReactComponent as FacebookIcon } from '../../assets/svg/facebook.svg'
 import { makeStyles } from '@material-ui/core'
 
 interface FooterProps {
@@ -18,6 +19,7 @@ const useStyles = makeStyles(() => ({
     background: '#333',
     padding: '30px 10% 15px 10%'
   },
+  wrapper: {},
   contact: {
     width: 450,
     marginRight: 20,
@@ -99,6 +101,37 @@ const useStyles = makeStyles(() => ({
     color: '#fff',
     fontWeight: 500,
     fontSize: 20
+  },
+  subFooter: {
+    backgroundColor: '#252121',
+    padding: '10px 10%',
+    color: '#a29e9e',
+    textAlign: 'center',
+    '& > span': {
+      fontSize: 12
+    }
+  },
+  socialMedia: {
+    margin: 0,
+    padding: 0,
+    listStyle: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: 5
+  },
+  socialMediaIcon: {
+    fontSize: '15px',
+    fill: '#A0A0A0',
+    marginRight: 8,
+    transition: 'fill 0.2s',
+
+    '&:hover': {
+      fill: '#ff9900'
+    },
+    '& svg': {
+      width: 20,
+      height: 20
+    }
   }
 }))
 
@@ -106,68 +139,96 @@ const Footer: React.FC<FooterProps> = ({ themeChanger }) => {
   const classes = useStyles()
 
   return (
-    <footer className={classes.root}>
-      {/* <Japer /> */}
-      {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <div className={classes.wrapper}>
+      <footer className={classes.root}>
+        {/* <Japer /> */}
+        {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
         <NightToggleSwith themeChanger={themeChanger} />
       </div> */}
-      <div className={classes.contact}>
-        <Typography component="h5">Связь с нами</Typography>
-        <p className={classes.contactMessage}>
-          Привет, мы всегда открыты для сотрудничества и предложений, свяжитесь с нами одним из способов ниже
-        </p>
-        <ul className={classes.contactDetails}>
-          <li>
-            <Typography component="p">Телефон</Typography>
-            <Typography component="span">380 ** *** ****</Typography>
-          </li>
-          <li>
-            <Typography component="p">Эл. почта</Typography>
-            <Typography component="span">test-mail@some.com</Typography>
-          </li>
-          <li>
-            <Typography component="p">Рабочие часы</Typography>
-            <Typography component="span">Пн-Вс, 9:00-20:00</Typography>
-          </li>
-          <li>
-            <Typography component="p">Мы находимся</Typography>
-            <Typography component="span">г. Харьков</Typography>
-          </li>
-        </ul>
-      </div>
-      <div className={classes.information}>
-        <Typography component="h5">Информация</Typography>
-        <Link to="#">
-          <Typography component="p">Доставка</Typography>
-        </Link>
-        <Link to="#">
-          <Typography component="p">Обратная связь</Typography>
-        </Link>
-        <Link to="#">
-          <Typography component="p">Возврат</Typography>
-        </Link>
-        <Link to="#">
-          <Typography component="p">Политика конфиденциальности</Typography>
-        </Link>
-      </div>
-      <div className={classes.brandSection}>
-        <Link to="#">
-          <Typography className={classes.logo} component="h1">
-            Bags
-            <sup>2</sup>
-            on
-          </Typography>
-        </Link>
-        <div className={classes.logoBox}>
-          <Icon className={classes.icon}>
-            <PhoneIcon />
-          </Icon>
-          <Typography component="p" className={classes.bigNumber}>
-            38(099)-111-1111
-          </Typography>
+        <div className={classes.contact}>
+          <Typography component="h5">Связь с нами</Typography>
+          <p className={classes.contactMessage}>
+            Привет, мы всегда открыты для сотрудничества и предложений, свяжитесь с нами одним из способов ниже
+          </p>
+          <ul className={classes.contactDetails}>
+            <li>
+              <Typography component="p">Телефон</Typography>
+              <Typography component="span">380 ** *** ****</Typography>
+            </li>
+            <li>
+              <Typography component="p">Эл. почта</Typography>
+              <Typography component="span">test-mail@some.com</Typography>
+            </li>
+            <li>
+              <Typography component="p">Рабочие часы</Typography>
+              <Typography component="span">Пн-Вс, 9:00-20:00</Typography>
+            </li>
+            <li>
+              <Typography component="p">Мы находимся</Typography>
+              <Typography component="span">г. Харьков</Typography>
+            </li>
+          </ul>
         </div>
+        <div className={classes.information}>
+          <Typography component="h5">Информация</Typography>
+          <Link to="#">
+            <Typography component="p">Доставка</Typography>
+          </Link>
+          <Link to="#">
+            <Typography component="p">Обратная связь</Typography>
+          </Link>
+          <Link to="#">
+            <Typography component="p">Возврат</Typography>
+          </Link>
+          <Link to="#">
+            <Typography component="p">Политика конфиденциальности</Typography>
+          </Link>
+        </div>
+        <div className={classes.brandSection}>
+          <Link to="#">
+            <Typography className={classes.logo} component="h1">
+              Bags
+              <sup>2</sup>
+              on
+            </Typography>
+          </Link>
+          <div className={classes.logoBox}>
+            <Icon className={classes.icon}>
+              <PhoneIcon />
+            </Icon>
+            <Typography component="p" className={classes.bigNumber}>
+              38(099)-111-1111
+            </Typography>
+          </div>
+          <ul className={classes.socialMedia}>
+            <li>
+              <Link to="#">
+                <Icon className={classes.socialMediaIcon}>
+                  <InstagramIcon />
+                </Icon>
+              </Link>
+            </li>
+            <li>
+              <Link to="#">
+                <Icon className={classes.socialMediaIcon}>
+                  <TelegramIcon />
+                </Icon>
+              </Link>
+            </li>
+            <li>
+              <Link to="#">
+                <Icon className={classes.socialMediaIcon}>
+                  <FacebookIcon />
+                </Icon>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </footer>
+      <div className={classes.subFooter}>
+        <Typography component="span">Bags2on © 2021</Typography>
       </div>
-    </footer>
+    </div>
   )
 }
 
