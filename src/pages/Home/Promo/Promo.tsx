9072b10398a49db3816b44b065508486811604cb
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-03-18 13:00:03 +0200

    promo banner updates

--- src/pages/Home/Promo/Promo.tsx
+++ src/pages/Home/Promo/Promo.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import firstImage from '../../../assets/rastr/block_one.png'
+import mBannerImage from '../../../assets/rastr/m-banner.jpeg'
+import wBannerImage from '../../../assets/rastr/w-banner.jpeg'
 import { makeStyles } from '@material-ui/core'
-
-interface PromoProps {}
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,31 +10,60 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'space-between',
     flexWrap: 'wrap'
   },
-  box: {
-    flexBasis: '100%',
+  wrapper: {
+    overflow: 'hidden',
     padding: '10px 0',
-    '& > img': {
-      width: '100%',
-      height: '100%',
-      userSelect: 'none'
-    },
+    flexBasis: '100%',
     [theme.breakpoints.up('md')]: {
       flexBasis: '49%',
       padding: 0
     }
+  },
+  box: {
+    width: '100%',
+    height: '100%',
+    cursor: 'pointer',
+    overflow: 'hidden',
+    '&:hover': {
+      '& > $image': {
+        transform: 'scale(1.2)'
+      }
+    }
+  },
+  title: {
+    color: 'orange',
+    fontWeight: 600,
+    fontSize: 25
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    userSelect: 'none',
+    transition: 'all 0.3s'
   }
 }))
 
-const Promo: React.FC<PromoProps> = () => {
+const Promo: React.FC = () => {
   const classes = useStyles()
+  {
+    /*
+      <p className={classes.title}>Для Мужчин</p>
+      <p className={classes.title}>Для Женщин</p>
+      <img src={wBannerImage} className={classes.image} alt="one" />
+    */
+  }
 
   return (
     <section className={classes.root}>
-      <div className={classes.box}>
-        <img src={firstImage} alt="one" />
+      <div className={classes.wrapper}>
+        <div className={classes.box}>
+          <img src={wBannerImage} className={classes.image} alt="one" />
+        </div>
       </div>
-      <div className={classes.box}>
-        <img src={firstImage} alt="two" />
+      <div className={classes.wrapper}>
+        <div className={classes.box}>
+          <img src={mBannerImage} className={classes.image} alt="one" />
+        </div>
       </div>
     </section>
   )
