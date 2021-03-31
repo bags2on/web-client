d63642b81b67f5fd98c8bce42195bc9efa7f6617
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-03-31 17:55:56 +0300

    validate on click

--- src/pages/Home/JoinUs/JoinUs.tsx
+++ src/pages/Home/JoinUs/JoinUs.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import clsx from 'clsx'
 import Button from '../../../shared/Button'
 import { JoinUsSchema, JoinUsSchemaType } from '../../../utils/validationSchema'
 import { Formik, Form, Field } from 'formik'
@@ -61,6 +62,7 @@ const useStyles = makeStyles((theme) => ({
     borderColor: theme.palette.type === 'light' ? '#c4c4c4' : '#3c4144',
     backgroundColor: theme.palette.type === 'light' ? '#fff' : '#3c4144',
     padding: '10px 20px 10px 15px',
+    transition: 'all 300ms',
     '&::placeholder': {
       color: theme.palette.type === 'light' ? '#3c4144' : '#c4c4c4'
     },
@@ -73,6 +75,9 @@ const useStyles = makeStyles((theme) => ({
       width: 320
     }
   },
+  inputError: {
+    animation: '$error-blink 300ms ease-in-out'
+  },
   submitButton: {
     width: 193,
     margin: '0 auto',
@@ -83,6 +88,17 @@ const useStyles = makeStyles((theme) => ({
       borderTopLeftRadius: 0,
       borderBottomLeftRadius: 0
     }
+  },
+  '@keyframes error-blink': {
+    '0%': {
+      borderColor: theme.palette.type === 'light' ? '#c4c4c4' : '#3c4144'
+    },
+    '50%': {
+      borderColor: '#FF502B'
+    },
+    '100%': {
+      borderColor: theme.palette.type === 'light' ? '#c4c4c4' : '#3c4144'
+    }
   }
 }))
 
@@ -90,6 +106,8 @@ const JoinUs: React.FC = () => {
   const classes = useStyles()
 
   const handleSubmit = (values: JoinUsSchemaType): void => {
+    console.log('submit')
+
     console.log(values)
   }
 
@@ -101,15 +119,29 @@ const JoinUs: React.FC = () => {
           ...и получайте скидки в <b>личном кабинете</b>
         </p>
         <Formik
+          validateOnBlur={false}
+          validateOnChange={false}
           onSubmit={handleSubmit}
           initialValues={{
             email: ''
           }}
           validationSchema={JoinUsSchema}
         >
-          {(): React.ReactElement => (
+          {({ errors, touched, setErrors }): React.ReactElement => (
             <Form className={classes.form} noValidate>
-              <Field name="email" autoComplete="off" placeholder="Введите ваш email" className={classes.input} />
+              {console.log(errors)}
+              <Field
+                onAnimationEnd={(): void => {
+                  setErrors({})
+                }}
+                name="email"
+                autoComplete="off"
+                placeholder="Введите ваш email"
+                className={clsx({
+                  [classes.input]: true,
+                  [classes.inputError]: errors.email && touched.email
+                })}
+              />
               <Button type="submit" className={classes.submitButton} withShadow={false} loading={false}>
                 Присоединиться
               </Button>
