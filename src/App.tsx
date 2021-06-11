c0621b9c902311dd3bbbfe01c414c1b4a88f5751
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-06-11 16:58:45 +0300

    '404' page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import routes from './utils/routes'
 import RootLayout from './components/RootLayout/RootLayout'
 import Fallback from './shared/Fallback'
@@ -8,14 +8,11 @@ const Home = lazy(() => import('./pages/Home'))
 const Catalog = lazy(() => import('./pages/Catalog/Catalog'))
 const Product = lazy(() => import('./pages/Product'))
 const Profile = lazy(() => import('./pages/Profile'))
+const NoMatchPage = lazy(() => import('./pages/NoMatchPage'))
 
 interface AppProps {
   themeChanger(checked: boolean): void
 }
-
-const isDev = process.env.NODE_ENV === 'development'
-
-const TEMP: React.FC = () => <div />
 
 const App: React.FC<AppProps> = ({ themeChanger }) => {
   return (
@@ -26,8 +23,7 @@ const App: React.FC<AppProps> = ({ themeChanger }) => {
           <Route path={routes.catalog} component={Catalog} />
           <Route exact path={routes.product} component={Product} />
           <Route exact path={routes.profile} component={Profile} />
-          {isDev && <Route exact path={'/clear'} component={TEMP} />}
-          <Redirect to={routes.root} />
+          <Route component={NoMatchPage} />
         </Switch>
       </RootLayout>
     </Suspense>
