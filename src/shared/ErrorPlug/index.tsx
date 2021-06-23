93fbc0b74d7e6dfc80ab27aefabb59e20822b573
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-06-23 12:58:46 +0300

    error plug with image

--- src/shared/ErrorPlug/index.tsx
+++ src/shared/ErrorPlug/index.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import Asset_2 from '../../assets/svg/Asset_2.svg'
 import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    height: 'calc(100 * var(--vh))',
+    height: 'calc(95 * var(--vh))',
     alignItems: 'center',
     justifyContent: 'center',
     flexGrow: 1
   },
+  imageWrapper: {
+    padding: '0 10px',
+    maxWidth: 230,
+    margin: '0 auto',
+    width: '100%',
+    '& > img': {
+      width: '100%'
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: 300
+    }
+  },
   title: {
     fontSize: 21,
+    marginTop: 50,
     [theme.breakpoints.up('md')]: {
       fontSize: 27
     }
@@ -34,6 +48,9 @@ const ErrorPlug: React.FC<ErrorPlugProps> = () => {
   return (
     <div className={classes.root}>
       <div style={{ textAlign: 'center' }}>
+        <div className={classes.imageWrapper}>
+          <img src={Asset_2} alt="asset 2" />
+        </div>
         <h1 className={classes.title}>Не удалось получить данные с сервера</h1>
         <p className={classes.subTitle}>попробуйте перезагрузить страницу</p>
       </div>
