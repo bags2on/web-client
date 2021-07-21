410676d4bb1fed0837773577127ba463778df01e
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-07-21 14:29:55 +0300

    Footer now without Typography

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import clsx from 'clsx'
-import Typography from '@material-ui/core/Typography'
 import Icon from '@material-ui/core/Icon'
 import { Link } from 'react-router-dom'
 import { ReactComponent as PhoneIcon } from '../../assets/svg/phone.svg'
@@ -24,9 +23,7 @@ const Footer: React.FC = () => {
             <Icon className={classes.icon}>
               <PhoneIcon />
             </Icon>
-            <Typography component="p" className={classes.phone}>
-              099&nbsp;123&nbsp;45&nbsp;67
-            </Typography>
+            <p className={classes.phone}>099&nbsp;123&nbsp;45&nbsp;67</p>
           </div>
           <ul className={classes.socialMedia}>
             <li>
@@ -52,88 +49,88 @@ const Footer: React.FC = () => {
             </li>
           </ul>
         </div>
-        <div className={clsx([classes.column, classes.category])}>
-          <Typography component="h5">Категории</Typography>
+        <div className={clsx(classes.column, classes.category)}>
+          <h5>Категории</h5>
           <ul>
             <li>
               <Link to="#">
-                <Typography component="p">Сумки</Typography>
+                <p>Сумки</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Кошельки</Typography>
+                <p>Кошельки</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Чемоданы</Typography>
+                <p>Чемоданы</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Женские</Typography>
+                <p>Женские</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Мужские</Typography>
+                <p>Мужские</p>
               </Link>
             </li>
           </ul>
         </div>
-        <div className={clsx([classes.column, classes.info])}>
-          <Typography component="h5">Информация</Typography>
+        <div className={clsx(classes.column, classes.info)}>
+          <h5>Информация</h5>
           <ul>
             <li>
               <Link to="#">
-                <Typography component="p">Доставка</Typography>
+                <p>Доставка</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Обратная связь</Typography>
+                <p>Обратная связь</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Возврат</Typography>
+                <p>Возврат</p>
               </Link>
             </li>
             <li>
               <Link to="#">
-                <Typography component="p">Политика конфиденциальности</Typography>
+                <p>Политика конфиденциальности</p>
               </Link>
             </li>
           </ul>
         </div>
         <div className={classes.contact}>
-          <Typography component="h5">Связь с нами</Typography>
+          <h5>Связь с нами</h5>
           <p className={classes.contactMessage}>
             Привет, мы всегда открыты для сотрудничества и предложений, свяжитесь с нами одним из способов ниже
           </p>
           <ul className={classes.contactDetails}>
             <li>
-              <Typography component="p">Телефон:</Typography>
-              <Typography component="span">099 123 45 67</Typography>
+              <p>Телефон:</p>
+              <span>099 123 45 67</span>
             </li>
             <li>
-              <Typography component="p">Эл. почта:</Typography>
-              <Typography component="span">test-mail@some.com</Typography>
+              <p>Эл. почта:</p>
+              <span>test-mail@some.com</span>
             </li>
             <li>
-              <Typography component="p">Рабочие часы:</Typography>
-              <Typography component="span">Пн-Вс, 9:00-20:00</Typography>
+              <p>Рабочие часы:</p>
+              <span>Пн-Вс, 9:00-20:00</span>
             </li>
             <li>
-              <Typography component="p">Мы находимся:</Typography>
-              <Typography component="span">г. Харьков</Typography>
+              <p>Мы находимся:</p>
+              <span>г. Харьков</span>
             </li>
           </ul>
         </div>
       </footer>
       <div className={classes.subFooter}>
-        <Typography component="span">{process.env.REACT_APP_NAME} © 2021</Typography>
+        <span>{process.env.REACT_APP_NAME} © 2021</span>
       </div>
     </div>
   )
