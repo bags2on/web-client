4783261719ef17bda2c2bdb70637b7baae4c3107
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-08-12 23:49:03 +0300

    react-image-magnifiers removed from app

--- src/pages/Product/Preview/Preview.tsx
+++ src/pages/Product/Preview/Preview.tsx
@@ -1,53 +1,22 @@
-import React, { useState } from 'react'
-import { SideBySideMagnifier } from 'react-image-magnifiers'
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-import './styles.scss'
+import classes from './styles.module.scss'
 
 interface PreviewProps {
   images: string[]
 }
 
 const Preview: React.FC<PreviewProps> = ({ images }) => {
-  const [currentImage, setCurrentImage] = useState<string>(images[0])
-
-  const switchSides = false
-  const overlayOpacity = 0.3
-  const alwaysInPlace = false
-
-  const handleImageChange = (url: string): void => {
-    setCurrentImage(url)
-  }
-
   return (
-    <div className="container">
-      <div className="magnifier-container">
-        <SideBySideMagnifier
-          className="side-magnifier"
-          style={{ order: switchSides ? 1 : 0 }}
-          imageSrc={currentImage}
-          alwaysInPlace={alwaysInPlace}
-          overlayOpacity={overlayOpacity}
-          switchSides={switchSides}
-          // largeImageSrc={largeImage} ???
-          // zoomPosition="left"
-          inPlaceMinBreakpoint={900}
-          fillAvailableSpace={true}
-          fillAlignTop={true}
-          fillGapTop={10}
-          fillGapRight={10}
-          fillGapBottom={10}
-          fillGapLeft={10}
-          zoomContainerBorder="none"
-          zoomContainerBoxShadow="0 4px 8px rgba(0,0,0,.5)"
-        />
-      </div>
-      <ul className="side-list">
-        {images.map((url: string, index: number) => (
-          <li key={index} onClick={() => handleImageChange(url)}>
-            <img src={url} alt={`фото продукта №${index + 1}`} className="side-list--image" />
-          </li>
+    <div className={classes.root}>
+      <Swiper loop navigation tag="div" wrapperTag="ul" slidesPerView={1} className={classes.swiperContainer}>
+        {images.map((image, index) => (
+          <SwiperSlide tag="li" key={index} virtualIndex={index}>
+            <img src={image} className={classes.slideImage} alt={`фото продукта №${index + 1}`} />
+          </SwiperSlide>
         ))}
-      </ul>
+      </Swiper>
     </div>
   )
 }
