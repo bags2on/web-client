269d49a534587ea02630f2cb31cf174da47adc39
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-09-13 12:34:44 +0300

    home promo section updated

--- src/pages/Home/Promo/Promo.tsx
+++ src/pages/Home/Promo/Promo.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import maleBanner from '../../../assets/rastr/m-banner.jpeg'
-import femaleBanner from '../../../assets/rastr/f-banner.jpeg'
 import routes from '../../../utils/routes'
+import manImage from '../../../assets/svg/man.svg'
+import womanImage from '../../../assets/svg/woman.svg'
 import { Link } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { makeStyles } from '@material-ui/core'
@@ -9,70 +9,72 @@ import { makeStyles } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    padding: '20px 5px 10px 5px',
+    padding: '0 5px 10px 5px',
     justifyContent: 'space-between',
     flexWrap: 'wrap'
   },
   wrapper: {
+    textDecoration: 'none',
+    marginTop: 10,
+    display: 'flex',
     overflow: 'hidden',
-    margin: '10px 0',
     flexBasis: '100%',
-    position: 'relative',
-    '&:hover': {
-      '& $image': {
-        transform: 'scale(1.2)'
-      },
-      '& $overlay': {
-        background: 'linear-gradient(to top, hsla(0, 0%, 0%, 0.9) 6%, hsla(0, 0%, 0%, 0) 50%)'
-      },
-      '& $fakeButton': {
-        color: '#fff',
-        backgroundColor: theme.palette.secondary.main
-      }
-    },
+    height: 250,
+    borderRadius: 6,
+    boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+    backgroundColor: theme.palette.type === 'light' ? '#fff' : '#363636',
+    '-webkit-tap-highlight-color': 'transparent',
     [theme.breakpoints.up('md')]: {
       flexBasis: '49%',
-      padding: 0
+      padding: 0,
+      '&:hover': {
+        '& $image': {
+          transform: 'scale(1.1)'
+        }
+      }
     }
   },
-  overlay: {
-    zIndex: 10,
-    position: 'absolute',
-    bottom: 0,
-    top: 0,
-    left: 0,
-    right: 0,
-    background: 'linear-gradient(to top, hsla(0, 0%, 0%, 0.6) 6%, hsla(0, 0%, 0%, 0) 50%)',
-    textDecoration: 'none',
-    '-webkit-tap-highlight-color': 'transparent'
-  },
   contentBox: {
-    marginLeft: 25,
-    marginTop: '43%'
+    width: '100%',
+    paddingTop: 75
+  },
+  image: {
+    maxWidth: 175,
+    userSelect: 'none',
+    transition: 'all 0.3s',
+    '& > img': {
+      width: '100%',
+      height: '100%'
+    }
   },
   title: {
     fontWeight: 500,
     fontSize: 23,
     margin: 0,
-    color: '#fff',
+    textAlign: 'center',
+    color: theme.palette.type === 'light' ? '#343434' : '#fff',
     userSelect: 'none'
   },
-  image: {
-    width: '100%',
-    height: '100%',
-    userSelect: 'none',
-    transition: 'all 0.2s'
+  buttonWrapper: {
+    marginTop: 17,
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'center'
   },
   fakeButton: {
     display: 'inline-block',
-    marginTop: 7,
     fontWeight: 600,
     padding: '8px 10px',
     borderRadius: 16,
-    color: '#343434',
-    backgroundColor: '#fff',
+    border: '1px solid',
+    borderColor: theme.palette.type === 'light' ? '#343434' : theme.palette.secondary.main,
+    color: theme.palette.type === 'light' ? '#343434' : '#fff',
     textTransform: 'uppercase',
-    transition: 'all 0.2s'
+    transition: 'all 0.2s',
+    '&:hover': {
+      backgroundColor: theme.palette.type === 'light' ? '#343434' : theme.palette.secondary.main,
+      color: theme.palette.type === 'light' ? '#fff' : '#343434'
+    }
   }
 }))
 
@@ -82,40 +84,44 @@ const Promo: React.FC = () => {
 
   return (
     <section className={classes.root}>
-      <div className={classes.wrapper}>
-        <Link
-          to={{
-            pathname: routes.catalog,
-            state: {
-              genderType: 'Male'
-            }
-          }}
-          className={classes.overlay}
-        >
-          <div className={classes.contentBox}>
-            <p className={classes.title}>{t('home.promo.male')}</p>
+      <Link
+        to={{
+          pathname: routes.catalog,
+          state: {
+            genderType: 'Male'
+          }
+        }}
+        className={classes.wrapper}
+      >
+        <div className={classes.image}>
+          <img src={womanImage} alt={`банер: '${t('home.promo.female')}'`} />
+        </div>
+        <div className={classes.contentBox}>
+          <p className={classes.title}>{t('home.promo.female')}</p>
+          <div className={classes.buttonWrapper}>
             <div className={classes.fakeButton}>{t('home.promo.action')}</div>
           </div>
-        </Link>
-        <img src={maleBanner} className={classes.image} alt="банер" />
-      </div>
-      <div className={classes.wrapper}>
-        <Link
-          to={{
-            pathname: routes.catalog,
-            state: {
-              genderType: 'Female'
-            }
-          }}
-          className={classes.overlay}
-        >
-          <div className={classes.contentBox}>
-            <p className={classes.title}>{t('home.promo.female')}</p>
+        </div>
+      </Link>
+      <Link
+        to={{
+          pathname: routes.catalog,
+          state: {
+            genderType: 'Female'
+          }
+        }}
+        className={classes.wrapper}
+      >
+        <div className={classes.image}>
+          <img src={manImage} alt={`банер: '${t('home.promo.male')}'`} />
+        </div>
+        <div className={classes.contentBox}>
+          <p className={classes.title}>{t('home.promo.male')}</p>
+          <div className={classes.buttonWrapper}>
             <div className={classes.fakeButton}>{t('home.promo.action')}</div>
           </div>
-        </Link>
-        <img src={femaleBanner} className={classes.image} alt="банер" />
-      </div>
+        </div>
+      </Link>
     </section>
   )
 }
