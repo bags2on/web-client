890e99d6f922832e4506204e38f00b6b02c0c1c6
Shevlyakov Vladislav <vladislavs319@gmail.com>
2021-11-02 16:11:33 +0200

    back-end connection via singe .env variable

--- src/apollo/apollo.ts
+++ src/apollo/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient } from '@apollo/client'
 import { cache } from './cache/cache'
 
-const API_GRAPHQL = process.env.REACT_APP_API_GRAPHQL
+const API_GRAPHQL = process.env.REACT_APP_API_URL + '/graphql'
 const withDevTools = process.env.NODE_ENV === 'development'
 
 const client = new ApolloClient({
