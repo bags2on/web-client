5d222b2c216eda49127dcabe1bac0a6c0032cb45
Shevlyakov Vladislav <vladislavs319@gmail.com>
2022-01-18 15:00:03 +0200

    previous cart functionality removed

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Drawer from '@material-ui/core/Drawer'
 import CartItems from './CartItems/CartItems'
-import Checkout from './Checkout/Checkout'
-import OrderSuccess from './OrderSuccess/OrderSuccess'
-import { useTransition, animated, config } from 'react-spring'
 import { makeStyles } from '@material-ui/core'
 import history from '../../utils/history'
 import routes from '../../utils/routes'
@@ -25,58 +22,15 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.up('md')]: {
       maxWidth: 400 // 363
     }
-  },
-  box: {
-    '& > div': {
-      cursor: 'pointer',
-      position: 'absolute',
-      width: '100%',
-      willChange: 'transform',
-      '-webkit-tap-highlight-color': 'transparent'
-    }
   }
 }))
 
 const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
   const classes = useStyles()
 
-  const pagesAmount = 3
-  const [[index, dir], setIndex] = useState<[number, number]>([0, 0])
-
-  const slideNext = () => setIndex([(index + 1) % pagesAmount, 1])
-  const slidePrev = () => setIndex([(index - 1 + pagesAmount) % pagesAmount, -1])
-
-  const transitions = useTransition(index, (p) => p, {
-    unique: true,
-    from: { transform: `translate3d(${dir === 1 ? 100 : -100}%,0,0)` },
-    enter: { transform: 'translate3d(0%,0,0)' },
-    leave: { transform: `translate3d(${dir === 1 ? -100 : 100}%,0,0)` },
-    config: config.default
-  })
-
   const handleCheckout = (): void => {
-    // slideNext()
-    onClose()
     history.push(routes.checkout)
   }
-
-  const handleConfirm = (): void => {
-    slideNext()
-  }
-  const handleToPrev = (): void => {
-    slidePrev()
-  }
-
-  const handleOrderFinish = () => {
-    setIndex([0, 0])
-    onClose()
-  }
-
-  const pages = [
-    <CartItems key={1} onClose={onClose} onCheckout={handleCheckout} />,
-    <Checkout key={2} onConfirm={handleConfirm} onBack={handleToPrev} />,
-    <OrderSuccess key={3} onClose={handleOrderFinish} />
-  ]
 
   return (
     <Drawer
@@ -89,18 +43,10 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClose }) => {
       }}
     >
       <div className={classes.root}>
-        {isOpen && (
-          <div className={classes.box}>
-            {transitions.map(({ item, props, key }) => (
-              <animated.div key={key} style={{ ...props }}>
-                {pages[item]}
-              </animated.div>
-            ))}
-          </div>
-        )}
+        {isOpen && <CartItems key={1} onClose={onClose} onCheckout={handleCheckout} />}
       </div>
     </Drawer>
   )
 }
 
-export default React.memo(Cart)
+export default Cart
