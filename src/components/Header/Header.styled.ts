23a77c50eadb5ab985c5c2f9a58ed79d53abf636
fresonn <vladislavs319@gmail.com>
2023-05-03 14:54:31 +0300

    Header component based styled-components

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,158 @@
+import styled, { css } from 'styled-components'
+import SvgIcon from '../../shared/SvgIcon'
+import IconButton from '@material-ui/core/IconButton'
+import { Link } from 'react-router-dom'
+
+export const TheHeader = styled.header`
+  position: fixed;
+  z-index: 500;
+  top: 0;
+  left: 0;
+  right: 0;
+  flex-wrap: wrap;
+  padding: 5px 0;
+  background-color: #282828;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.05), 0 1px 4px rgba(0, 0, 0, 0.05), 0 2px 8px rgba(0, 0, 0, 0.05);
+  @media ${({ theme }) => theme.media.lg} {
+    position: static;
+    padding: 10px 17px;
+  }
+  @media ${({ theme }) => theme.media.xl} {
+    padding: 13px 50px;
+  }
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 1500px;
+  margin: 0 auto;
+`
+
+export const LogoLink = styled(Link)`
+  display: none;
+  @media ${({ theme }) => theme.media.lg} {
+    display: block;
+    position: relative;
+    top: 3px;
+    width: 150px;
+    & > img {
+      width: 100%;
+      height: 100%;
+    }
+    @media ${({ theme }) => theme.media.xl} {
+      margin: 0 40px;
+    }
+  }
+`
+
+export const NavList = styled.ul`
+  display: none;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  & li {
+    padding-top: 8px;
+    padding-bottom: 8px;
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+  @media ${({ theme }) => theme.media.lg} {
+    display: flex;
+    align-items: center;
+  }
+`
+
+export const NavListLink = styled(Link)`
+  position: relative;
+  @media ${({ theme }) => theme.media.lg} {
+    font-size: 14px;
+    font-weight: 600;
+    text-decoration: none;
+    text-transform: uppercase;
+    color: ${({ theme }) => theme.colors.primary};
+    transition: color 0.3s;
+    &::after {
+      content: '';
+      position: absolute;
+      border-bottom: 2px solid;
+      border-bottom-color: ${({ theme }) => theme.colors.primary};
+      bottom: -7px;
+      left: 0;
+      right: 0;
+      opacity: 0;
+      visibility: hidden;
+      width: 0;
+      z-index: 1;
+      transition: all 0.3s ease;
+      -moz-transition: all 0.3s ease 0s;
+      -webkit-transition: all 0.3s ease 0s;
+      -o-transition: all 0.3s ease 0s;
+      -ms-transition: all 0.3s ease 0s;
+    }
+    &:hover {
+      &::after {
+        width: 45%;
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+  }
+`
+
+export const TheMenuIcon = styled(SvgIcon)`
+  fill: ${({ theme }) => theme.colors.primary};
+`
+
+export const TheHeartIcon = styled(SvgIcon)`
+  font-size: 24px;
+  fill: none;
+  stroke: ${({ theme }) => theme.colors.primary};
+  transition: all 0.3s;
+  &:hover {
+    transform: scale(1.2);
+  }
+`
+
+export const TheProfileIcon = styled(SvgIcon)`
+  font-size: 24px;
+  fill: ${({ theme }) => theme.colors.primary};
+  transition: all 0.3s;
+  &:hover {
+    transform: scale(1.2);
+  }
+`
+
+export const TheCartIcon = styled(SvgIcon)`
+  font-size: 27px;
+  fill: ${({ theme }) => theme.colors.primary};
+  stroke: ${({ theme }) => theme.colors.primary};
+  @media ${({ theme }) => theme.media.md} {
+    font-size: 24px;
+    transition: all 0.3s;
+    &:hover {
+      transform: scale(1.2);
+    }
+  }
+`
+
+const headerButton = css`
+  &:hover {
+    background: none;
+    & svg {
+      transition: all 0.3s;
+    }
+  }
+`
+export const HeaderButton = styled(IconButton)`
+  ${headerButton}
+`
+
+export const DynamicButton = styled(IconButton)`
+  display: none;
+  @media ${({ theme }) => theme.media.md} {
+    display: block;
+    padding: 11px;
+  }
+  ${headerButton}
+`
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,177 +1,43 @@
 import React from 'react'
-import clsx from 'clsx'
 import history from '../../utils/history'
 import routes from '../../utils/routes'
-import IconButton from '@material-ui/core/IconButton'
-import Badge from '@material-ui/core/Badge'
-import Icon from '@material-ui/core/Icon'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
+import Badge from '../../shared/Badge'
 import Search from '../../components/Search/Search'
+import logoImage from '../../assets/svg/logo.svg'
 import { ReactComponent as MenuIcon } from '../../assets/svg/icons/menu.svg'
 import { ReactComponent as HeartIcon } from '../../assets/svg/icons/heart.svg'
-import { ReactComponent as AvatarIcon } from '../../assets/svg/icons/avatar.svg'
+import { ReactComponent as ProfileIcon } from '../../assets/svg/icons/avatar.svg'
 import { ReactComponent as CartIcon } from '../../assets/svg/icons/header_cart.svg'
-import { Link } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
-import { makeStyles } from '@material-ui/core'
 import { GET_HEADER_DATA } from '../../apollo/cache/queries/shared'
 import { SharedMutations } from '../../apollo/cache/mutations'
 import { useTranslation } from 'react-i18next'
-import logoImage from '../../assets/svg/logo.svg'
 
-interface HeaderProps {
-  onDrawerOpen(): void
-  onCartOpen(): void
-}
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    position: 'fixed',
-    zIndex: 500,
-    top: 0,
-    left: 0,
-    right: 0,
-    flexWrap: 'wrap',
-    padding: '5px 0',
-    backgroundColor: '#282828',
-    boxShadow: '0 1px 2px rgba(0,0,0,0.05), 0 1px 4px rgba(0,0,0,0.05), 0 2px 8px rgba(0,0,0,0.05)',
-    [theme.breakpoints.up('lg')]: {
-      position: 'static',
-      padding: '10px 17px'
-    },
-    [theme.breakpoints.up('xl')]: {
-      padding: '13px 50px'
-    }
-  },
-  wrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    maxWidth: '1500px',
-    margin: '0 auto'
-  },
-  logo: {
-    display: 'none',
-    [theme.breakpoints.up('lg')]: {
-      display: 'block',
-      position: 'relative',
-      top: 3,
-      width: 150,
-      '& > img': {
-        width: '100%',
-        height: '100%'
-      }
-    },
-    [theme.breakpoints.up('xl')]: {
-      margin: '0 40px'
-    }
-  },
-  navList: {
-    display: 'none',
-    padding: 0,
-    [theme.breakpoints.up('lg')]: {
-      display: 'flex',
-      alignItems: 'center',
-      '& a': {
-        fontSize: 14,
-        fontWeight: 600,
-        textDecoration: 'none',
-        textTransform: 'uppercase',
-        color: theme.palette.secondary.main,
-        transition: 'color 0.3s',
-        position: 'relative',
-        '&::after': {
-          content: "''",
-          position: 'absolute',
-          borderBottom: '2px solid',
-          borderBottomColor: theme.palette.secondary.main,
-          bottom: -7,
-          left: 0,
-          right: 0,
-          opacity: 0,
-          visibility: 'hidden',
-          width: 0,
-          zIndex: 1,
-          transition: 'all .3s ease',
-          '-moz-transition': 'all .3s ease 0s',
-          '-webkit-transition': 'all .3s ease 0s',
-          '-o-transition': 'all .3s ease 0s',
-          '-ms-transition': 'all .3s ease 0s'
-        },
-        '&:hover': {
-          '&::after': {
-            width: '45%',
-            opacity: 1,
-            visibility: 'visible'
-          }
-        }
-      }
-    }
-  },
-  btns: {
-    '&:hover': {
-      background: 'none',
-      '& svg': {
-        transition: 'all 0.3s'
-      }
-    }
-  },
-
-  heartButton: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'block',
-      padding: 11
-    }
-  },
-  heartIcon: {
-    fontSize: 24,
-    fill: 'none',
-    stroke: theme.palette.secondary.main,
-    transition: 'all 0.3s',
-    '&:hover': {
-      transform: 'scale(1.2)'
-    }
-  },
-  avatarIcon: {
-    fontSize: 24,
-    fill: theme.palette.secondary.main,
-    transition: 'all 0.3s',
-    '&:hover': {
-      transform: 'scale(1.2)'
-    }
-  },
-  cartIcon: {
-    fontSize: 27,
-    fill: theme.palette.secondary.main,
-    stroke: theme.palette.secondary.main,
-    [theme.breakpoints.up('md')]: {
-      fontSize: 24,
-      transition: 'all 0.3s',
-      '&:hover': {
-        transform: 'scale(1.2)'
-      }
-    }
-  },
-  menuIcon: {
-    fill: theme.palette.secondary.main
-  },
-  toggl: {
-    marginLeft: 7,
-    paddingRight: 20,
-    [theme.breakpoints.down('sm')]: {
-      display: 'none'
-    }
-  }
-}))
+import {
+  TheHeader,
+  Wrapper,
+  LogoLink,
+  NavList,
+  NavListLink,
+  TheMenuIcon,
+  TheHeartIcon,
+  TheProfileIcon,
+  TheCartIcon,
+  HeaderButton,
+  DynamicButton
+} from './Header.styled'
 
 interface HeaderQuery {
   cartAmount: number
   favoriteAmount: number
 }
 
+interface HeaderProps {
+  onDrawerOpen(): void
+  onCartOpen(): void
+}
+
 const Header: React.FC<HeaderProps> = ({ onDrawerOpen, onCartOpen }) => {
-  const classes = useStyles()
   const { data } = useQuery<HeaderQuery>(GET_HEADER_DATA)
   const { t } = useTranslation()
 
@@ -194,66 +60,52 @@ const Header: React.FC<HeaderProps> = ({ onDrawerOpen, onCartOpen }) => {
   }
 
   return (
-    <header className={classes.root}>
-      <div className={classes.wrapper}>
-        <IconButton onClick={onDrawerOpen} disableRipple className={classes.btns}>
-          <Icon
-            classes={{
-              root: classes.menuIcon
-            }}
-          >
+    <TheHeader>
+      <Wrapper>
+        <HeaderButton onClick={onDrawerOpen} disableRipple>
+          <TheMenuIcon>
             <MenuIcon />
-          </Icon>
-        </IconButton>
-        <Link to="/" className={classes.logo}>
+          </TheMenuIcon>
+        </HeaderButton>
+        <LogoLink to="/">
           <img src={logoImage} alt="логотип" />
-        </Link>
+        </LogoLink>
         <nav>
-          <List className={classes.navList}>
-            <ListItem component="li">
-              <Link to={routes.root}>{t('header.home')}</Link>
-            </ListItem>
-            <ListItem component="li">
-              <Link to={routes.catalog}>Каталог</Link>
-            </ListItem>
-          </List>
+          <NavList>
+            <li>
+              <NavListLink to={routes.root}>{t('header.home')}</NavListLink>
+            </li>
+            <li>
+              <NavListLink to={routes.catalog}>Каталог</NavListLink>
+            </li>
+          </NavList>
         </nav>
         {/*  */}
         <Search />
         {/*  */}
-        <IconButton
-          color="primary"
-          onClick={handleFavoritesClick}
-          disableRipple
-          className={clsx(classes.btns, classes.heartButton)}
-        >
-          <Badge badgeContent={favoriteAmount} max={999} color="error">
-            <Icon className={classes.heartIcon}>
+        <DynamicButton color="primary" onClick={handleFavoritesClick} disableRipple>
+          <Badge content={favoriteAmount}>
+            <TheHeartIcon>
               <HeartIcon />
-            </Icon>
+            </TheHeartIcon>
           </Badge>
-        </IconButton>
-        <IconButton
-          color="primary"
-          onClick={handleProfileClick}
-          disableRipple
-          className={clsx(classes.btns, classes.heartButton)}
-        >
-          <Badge badgeContent={0} max={999} color="error">
-            <Icon className={classes.avatarIcon}>
-              <AvatarIcon />
-            </Icon>
+        </DynamicButton>
+        <DynamicButton color="primary" onClick={handleProfileClick} disableRipple>
+          <Badge content={0} max={5}>
+            <TheProfileIcon>
+              <ProfileIcon />
+            </TheProfileIcon>
           </Badge>
-        </IconButton>
-        <IconButton color="primary" onClick={handleCartClick} disableRipple className={classes.btns}>
-          <Badge badgeContent={cartAmount} max={999} color="error">
-            <Icon className={classes.cartIcon}>
+        </DynamicButton>
+        <HeaderButton color="primary" onClick={handleCartClick} disableRipple>
+          <Badge content={cartAmount}>
+            <TheCartIcon>
               <CartIcon />
-            </Icon>
+            </TheCartIcon>
           </Badge>
-        </IconButton>
-      </div>
-    </header>
+        </HeaderButton>
+      </Wrapper>
+    </TheHeader>
   )
 }
 
