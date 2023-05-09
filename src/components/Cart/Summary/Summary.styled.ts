77d259e35ed1bc6ec92a62e9bec4fcd19b1b479e
fresonn <vladislavs319@gmail.com>
2023-05-09 13:28:58 +0300

    cart summary styled-components based

---
+++ src/components/Cart/Summary/Summary.styled.ts
@@ -0,0 +1,48 @@
+import styled, { css } from 'styled-components'
+import IconButton from '../../../shared/IconButton'
+import SvgIcon from '../../../shared/SvgIcon'
+
+export const Container = styled.div`
+  position: relative;
+  padding: 15px 10px 25px 15px;
+  background-color: ${({ theme }) => (theme.type === 'light' ? '#f3f3f3' : '#282828')};
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  align-items: center;
+  margin-bottom: 10px;
+`
+
+export const InnerContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`
+
+export const CloseButton = styled(IconButton)<{ $loading?: boolean }>`
+  color: #8c8c8c;
+  margin-right: 30px;
+  ${({ $loading }) =>
+    $loading &&
+    css`
+      position: absolute;
+      top: 18px;
+    `}
+`
+
+export const TheCloseIcon = styled(SvgIcon)`
+  fill: '#373737';
+  font-size: 17px;
+`
+
+export const CartPrice = styled.p`
+  margin: 0;
+  font-size: 18px;
+  & > span {
+    font-size: 16px;
+    font-weight: 500;
+  }
+`
--- src/components/Cart/Summary/Summary.tsx
+++ src/components/Cart/Summary/Summary.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import clsx from 'clsx'
-import Grid from '@material-ui/core/Grid'
-import CloseIcon from '@material-ui/icons/Close'
-import IconButton from '@material-ui/core/IconButton'
-import ContentLoader from 'react-content-loader'
 import Button from '../../../shared/Button/Button'
+import ContentLoader from 'react-content-loader'
+import { ReactComponent as CrossIcon } from '../../../assets/svg/icons/cross.svg'
 import { useReactiveVar } from '@apollo/client'
 import { cartPriceVar } from '../../../apollo/cache/variables'
 import { formatPrice } from '../../../utils/helpers'
-import { makeStyles } from '@material-ui/core'
+
+import { CloseButton, Container, InnerContainer, TheCloseIcon, Wrapper, CartPrice } from './Summary.styled'
 
 interface SummaryProps {
   isLoading: boolean
@@ -16,53 +14,7 @@ interface SummaryProps {
   onCheckout(): void
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '15px 10px 25px 15px',
-    backgroundColor: theme.palette.type === 'light' ? '#f3f3f3' : '#282828'
-  },
-  topContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    width: '100%', // ?
-    alignItems: 'center',
-    marginBottom: 10
-  },
-  closeCartButton: {
-    marginRight: 30
-  },
-  checkoutButton: {
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'block',
-    width: '100%',
-    fontWeight: 500,
-    fontSize: 18,
-    color: '#fff',
-    padding: '10px 0',
-    marginTop: 10,
-    backgroundColor: theme.palette.secondary.main,
-    '&:hover': {
-      backgroundColor: '#ffa51e'
-    }
-  },
-  skeletonCheckoutBtn: {
-    position: 'absolute',
-    top: 13
-  },
-  totalTitle: {
-    margin: 0,
-    fontSize: 18,
-    '& > span': {
-      fontSize: 16,
-      fontWeight: 500
-    }
-  }
-}))
-
 const Summary: React.FC<SummaryProps> = ({ isLoading, onClose, onCheckout }) => {
-  const classes = useStyles()
-
   const cartPrice = useReactiveVar(cartPriceVar)
 
   const handleCheckoutClick = (): void => {
@@ -72,43 +24,39 @@ const Summary: React.FC<SummaryProps> = ({ isLoading, onClose, onCheckout }) => 
 
   if (isLoading) {
     return (
-      <div className={classes.root}>
-        <IconButton onClick={onClose} className={clsx(classes.closeCartButton, classes.skeletonCheckoutBtn)}>
-          <CloseIcon />
-        </IconButton>
-        <ContentLoader
-          backgroundColor="#F2E30C"
-          foregroundColor="#ffd9a3"
-          width="100%"
-          height="102"
-          viewBox="0 0 375 102"
-        >
-          <rect x="80" y="12" rx="4" ry="4" width="142" height="22" />
+      <Container>
+        <CloseButton $loading={isLoading} onClick={onClose} disableRipple>
+          <TheCloseIcon>
+            <CrossIcon />
+          </TheCloseIcon>
+        </CloseButton>
+        <ContentLoader backgroundColor="#F2E30C" foregroundColor="#ffd9a3" width="100%" height="102">
+          <rect x="70" y="12" rx="4" ry="4" width="142" height="25" />
           <rect x="0" y="57" rx="8" ry="8" width="100%" height="44" />
         </ContentLoader>
-      </div>
+      </Container>
     )
   }
 
   return (
-    <section className={classes.root}>
-      <Grid container>
-        <div className={classes.topContainer}>
-          <IconButton onClick={onClose} className={classes.closeCartButton}>
-            <CloseIcon />
-          </IconButton>
-          <p className={classes.totalTitle}>
+    <Container>
+      <InnerContainer>
+        <Wrapper>
+          <CloseButton disableRipple onClick={onClose}>
+            <TheCloseIcon>
+              <CrossIcon />
+            </TheCloseIcon>
+          </CloseButton>
+          <CartPrice>
             Итого:&nbsp;
             <span>{formatPrice(cartPrice)}&nbsp;грн.</span>
-          </p>
-        </div>
-        <Grid container>
-          <Button fullWidth disableShadow color="secondary" onClick={handleCheckoutClick}>
-            Оформить заказ
-          </Button>
-        </Grid>
-      </Grid>
-    </section>
+          </CartPrice>
+        </Wrapper>
+        <Button fullWidth disableShadow color="secondary" onClick={handleCheckoutClick}>
+          Оформить заказ
+        </Button>
+      </InnerContainer>
+    </Container>
   )
 }
 
