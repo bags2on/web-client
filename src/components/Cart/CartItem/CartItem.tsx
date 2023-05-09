60b1b9de8738cf9d01d4406e341a472803b38fa7
fresonn <vladislavs319@gmail.com>
2023-05-09 17:30:26 +0300

    cart item styled-components based

--- src/components/Cart/CartItem/CartItem.tsx
+++ src/components/Cart/CartItem/CartItem.tsx
@@ -1,7 +1,4 @@
 import React, { useState } from 'react'
-import Grid from '@material-ui/core/Grid'
-import IconButton from '@material-ui/core/IconButton'
-import SvgIcon from '@material-ui/core/SvgIcon'
 import ImagePlaceholder from '../../../shared/ImagePlaceholder'
 import AmountController from '../../../shared/AmountController'
 import routes from '../../../utils/routes'
@@ -9,8 +6,19 @@ import { Link } from 'react-router-dom'
 import { formatPrice } from '../../../utils/helpers'
 import { generateLink } from '../../../utils/links'
 import { CartMutations } from '../../../apollo/cache/mutations'
-import { ReactComponent as TrashIcon } from '../../../assets/svg/icons/trash-alt.svg'
-import { makeStyles } from '@material-ui/core'
+import { ReactComponent as TrashIcon } from '../../../assets/svg/icons/trash.svg'
+
+import {
+  Container,
+  ImageWrapper,
+  Info,
+  Title,
+  Price,
+  Controls,
+  Amount,
+  DeleteButton,
+  TheTrashIcon
+} from './CartItem.styled'
 
 export type CartItemType = {
   id: string
@@ -25,86 +33,8 @@ interface CartItemProps {
   onRemove: (id: string) => void
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    position: 'relative',
-    marginBottom: 35,
-    '&::after': {
-      content: "''",
-      position: 'absolute',
-      width: '60%',
-      height: '2px',
-      borderRadius: 'inherit',
-      background: '#dcdcdc',
-      bottom: '-27px',
-      left: '50%',
-      transform: 'translate(-50%, -50%)'
-    }
-  },
-  box: {
-    width: '100%',
-    minWidth: '100px',
-    maxWidth: '180px',
-    position: 'relative',
-    marginRight: 20
-  },
-  productInfoBox: {
-    width: '100%',
-    maxWidth: '900px',
-    paddingTop: '5%'
-  },
-  title: {
-    maxWidth: 200,
-    margin: 0,
-    fontSize: 19,
-    fontWeight: 500,
-    paddingBottom: 4,
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    color: 'inherit',
-    textDecoration: 'none',
-    transition: 'color .2s',
-    display: 'inline-block',
-    '&:hover, &:focus': {
-      color: 'orange',
-      outline: 'none'
-    }
-  },
-  priceTitle: {
-    color: '#707070',
-    fontSize: 16,
-    fontWeight: 500
-  },
-  amountTitle: {
-    margin: 0,
-    paddingLeft: 1,
-    fontSize: 16,
-    fontWeight: 500
-  },
-  trashButton: {
-    color: '#a7a7a7',
-    fontSize: 12,
-    borderRadius: 10,
-    padding: '0px 17px',
-    backgroundColor: theme.palette.type === 'light' ? '#ebebeb' : '#363636',
-    '&:hover': {
-      color: '#dc143c',
-      backgroundColor: theme.palette.type === 'light' ? '#ebebeb' : '#363636'
-    }
-  },
-  controllerWrapper: {
-    marginTop: 20,
-    display: 'flex',
-    justifyContent: 'space-between'
-  }
-}))
-
 const CartItem: React.FC<CartItemProps> = ({ product, onRemove }) => {
   const { id, title, preview, currentPrice, amount } = product
-
-  const classes = useStyles()
   const [count, setCount] = useState<number>(amount)
 
   const handleAmountChange = (_: string, n: number): void => {
@@ -112,48 +42,35 @@ const CartItem: React.FC<CartItemProps> = ({ product, onRemove }) => {
     setCount(n)
   }
 
-  const handleProductRemove = (): void => {
+  const handleProductRemove = () => {
     onRemove(id)
   }
 
   return (
-    <div className={classes.root}>
-      <div className={classes.box}>
+    <Container>
+      <ImageWrapper>
         <Link to={generateLink(routes.product, id)}>
           <ImagePlaceholder plain src={preview} altText={title} />
         </Link>
-      </div>
-      <div className={classes.productInfoBox}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Link title={title} to={generateLink(routes.product, id)} className={classes.title}>
-              {title}
-            </Link>
-          </Grid>
-          <Grid item xs={12}>
-            <span className={classes.priceTitle}>Цена:&nbsp;&nbsp;{formatPrice(currentPrice)}&nbsp;₴</span>
-          </Grid>
-          <Grid item xs={12}>
-            <p className={classes.amountTitle}>
-              {count}&nbsp;шт:&nbsp;&nbsp;{formatPrice(count * currentPrice)}&nbsp;грн.
-            </p>
-          </Grid>
-        </Grid>
-        <div className={classes.controllerWrapper}>
+      </ImageWrapper>
+      <Info>
+        <Title title={title} to={generateLink(routes.product, id)}>
+          {title}
+        </Title>
+        <Price>Цена:&nbsp;&nbsp;{formatPrice(currentPrice)}&nbsp;₴</Price>
+        <Amount>
+          {count}&nbsp;шт:&nbsp;&nbsp;{formatPrice(count * currentPrice)}&nbsp;грн.
+        </Amount>
+        <Controls>
           <AmountController min={1} max={100} amount={count} onChange={handleAmountChange} />
-          <IconButton
-            disableRipple
-            onClick={handleProductRemove}
-            className={classes.trashButton}
-            aria-label={`Удалить "${title}"`}
-          >
-            <SvgIcon component="span" fontSize="small">
+          <DeleteButton disableRipple onClick={handleProductRemove} aria-label={`Удалить "${title}"`}>
+            <TheTrashIcon>
               <TrashIcon />
-            </SvgIcon>
-          </IconButton>
-        </div>
-      </div>
-    </div>
+            </TheTrashIcon>
+          </DeleteButton>
+        </Controls>
+      </Info>
+    </Container>
   )
 }
 
