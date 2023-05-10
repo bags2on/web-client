b65d6a73c9d26c038cd2d309bd4fe0f8736e970c
fresonn <vladislavs319@gmail.com>
2023-05-10 19:00:25 +0300

    checkout cart preview based on "styled"

--- src/pages/Checkout/Preview/Preview.tsx
+++ src/pages/Checkout/Preview/Preview.tsx
@@ -12,46 +12,15 @@ import {
   CartProductsVariables
 } from '../../../graphql/product/_gen_/cartProducts.query'
 import type { CartItemType } from '../../../components/Cart/CartItem/CartItem'
-import { makeStyles } from '@material-ui/core'
+
+import { Container, Wrapper, LoaderWrapper } from './Preview.styled'
 
 interface PreviewProps {
   submitLoading: boolean
   orderCreationErr: boolean
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    padding: '10px',
-    [theme.breakpoints.up('lg')]: {
-      padding: 0
-    }
-  },
-  wrapper: {
-    backgroundColor: theme.palette.type === 'light' ? '#fff' : '#323232',
-    borderRadius: 14,
-    padding: '7px 10px 15px 10px',
-    [theme.breakpoints.up('lg')]: {
-      width: 430,
-      padding: '20px 40px 20px 30px'
-    }
-  },
-  loaderWrapper: {
-    width: '100%',
-    backgroundColor: theme.palette.type === 'light' ? '#fff' : '#323232',
-    borderRadius: 14,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '500px',
-    [theme.breakpoints.up('lg')]: {
-      width: 430
-    }
-  }
-}))
-
 const Preview: React.FC<PreviewProps> = ({ submitLoading, orderCreationErr }) => {
-  const classes = useStyles()
-
   const cartItems = useReactiveVar(cartItemsVar)
 
   const { data, loading, error } = useQuery<CartProductsQuery, CartProductsVariables>(CartProductsDocument, {
@@ -74,9 +43,9 @@ const Preview: React.FC<PreviewProps> = ({ submitLoading, orderCreationErr }) =>
 
   if (loading) {
     return (
-      <div className={classes.loaderWrapper}>
+      <LoaderWrapper>
         <ScaleLoader fallback />
-      </div>
+      </LoaderWrapper>
     )
   }
 
@@ -86,13 +55,13 @@ const Preview: React.FC<PreviewProps> = ({ submitLoading, orderCreationErr }) =>
   }
 
   return (
-    <section className={classes.root}>
-      <div className={classes.wrapper}>
+    <Container>
+      <Wrapper>
         <CartItems cartItems={data?.cartProducts || []} />
         <Summary submitLoading={submitLoading} orderCreationErr={orderCreationErr} />
-      </div>
+      </Wrapper>
       <SignupPromo />
-    </section>
+    </Container>
   )
 }
 
