d794b8fc105b759906f418f48a3cdf46081d3fa3
fresonn <vladislavs319@gmail.com>
2023-05-23 16:38:00 +0300

    404 page based on "styled"

--- src/pages/NoMatchPage/index.tsx
+++ src/pages/NoMatchPage/index.tsx
@@ -1,59 +1,27 @@
 import React from 'react'
 import history from '../../utils/history'
 import routes from '../../utils/routes'
-import Button from '../../shared/Button/Button'
 import NotFoundImage from '../../assets/svg/404.svg'
-import { makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 'calc(100 * var(--vh))'
-  },
-  container: {
-    width: '100%',
-    maxWidth: 500
-  },
-  title: {
-    margin: 0,
-    textAlign: 'center'
-  },
-  image: {
-    width: '100%',
-    maxWidth: 500,
-    '& img': {
-      width: '100%'
-    }
-  },
-  actionButton: {
-    display: 'block',
-    maxWidth: 150,
-    margin: '0 auto',
-    marginTop: 20
-  }
-}))
+import { Container, Inner, ImageBox, Title, ActionButton } from './NoMatchPage.styled'
 
 const NoMatchPage: React.FC = () => {
-  const classes = useStyles()
-
   const handleButtonClick = () => {
     history.push(routes.root)
   }
 
   return (
-    <div className={classes.root}>
-      <div className={classes.container}>
-        <div className={classes.image}>
+    <Container>
+      <Inner>
+        <ImageBox>
           <img src={NotFoundImage} alt="Страница не найдена (изображение)" />
-        </div>
-        <h1 className={classes.title}>Страница не найдена</h1>
-        <Button color="secondary" fullWidth className={classes.actionButton} onClick={handleButtonClick}>
+        </ImageBox>
+        <Title>Страница не найдена</Title>
+        <ActionButton color="secondary" fullWidth onClick={handleButtonClick}>
           На главную
-        </Button>
-      </div>
-    </div>
+        </ActionButton>
+      </Inner>
+    </Container>
   )
 }
 
