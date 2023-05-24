b3ac8c12e69fdd62b94b0b4f57df738e082a5824
fresonn <vladislavs319@gmail.com>
2023-05-24 12:25:14 +0300

    Error plug based on "styled"

--- src/shared/ErrorPlug/index.tsx
+++ src/shared/ErrorPlug/index.tsx
@@ -1,60 +1,23 @@
 import React from 'react'
 import Asset_2 from '../../assets/svg/Asset_2.svg'
-import { makeStyles } from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    height: 'calc(95 * var(--vh))',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexGrow: 1
-  },
-  imageWrapper: {
-    padding: '0 10px',
-    maxWidth: 230,
-    margin: '0 auto',
-    width: '100%',
-    '& > img': {
-      width: '100%'
-    },
-    [theme.breakpoints.up('lg')]: {
-      maxWidth: 300
-    }
-  },
-  title: {
-    fontSize: 21,
-    marginTop: 50,
-    [theme.breakpoints.up('md')]: {
-      fontSize: 27
-    }
-  },
-  subTitle: {
-    fontSize: 16,
-    color: 'var(--sub-title-color)',
-    [theme.breakpoints.up('md')]: {
-      fontSize: 20
-    }
-  }
-}))
+import { Container, ImageWrapper, Title, SubTitle } from './ErrorPlug.styled'
 
 interface ErrorPlugProps {
   message?: string
 }
 
 const ErrorPlug: React.FC<ErrorPlugProps> = () => {
-  const classes = useStyles()
-
   return (
-    <div className={classes.root}>
-      <div style={{ textAlign: 'center' }}>
-        <div className={classes.imageWrapper}>
+    <Container>
+      <div>
+        <ImageWrapper>
           <img src={Asset_2} alt="asset 2" />
-        </div>
-        <h1 className={classes.title}>Не удалось получить данные с сервера</h1>
-        <p className={classes.subTitle}>попробуйте перезагрузить страницу</p>
+        </ImageWrapper>
+        <Title>Не удалось получить данные с сервера</Title>
+        <SubTitle>попробуйте перезагрузить страницу</SubTitle>
       </div>
-    </div>
+    </Container>
   )
 }
 
