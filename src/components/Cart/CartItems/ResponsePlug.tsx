63c7b37957d573bea0d092220face64e17697326
fresonn <vladislavs319@gmail.com>
2023-05-24 12:14:34 +0300

    cat response plug based on "styled"

--- src/components/Cart/CartItems/ResponsePlug.tsx
+++ src/components/Cart/CartItems/ResponsePlug.tsx
@@ -1,63 +1,35 @@
 import React from 'react'
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
-import Button from '../../../shared/Button/Button'
 import { ReactComponent as EmptyCartIcon } from '../../../assets/svg/emptycart.svg'
-import { makeStyles } from '@material-ui/core'
+import { ReactComponent as ArrowIcon } from '../../../assets/svg/icons/expand-arrow.svg'
+
+import { Container, Content, BackButton, ExpandIcon } from './ResponsePlug.styled'
 
 interface ResponsePlugProps {
   text: string
   onClose(): void
 }
 
-const useStyles = makeStyles(() => ({
-  root: {
-    width: '100%',
-    height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column'
-  },
-  content: {
-    padding: '0 15px',
-    '& > svg': {
-      width: '100%',
-      height: 'auto',
-      fill: 'gray'
-    },
-    '& > p': {
-      margin: 0,
-      fontSize: 20,
-      fontWeight: 500,
-      textAlign: 'center'
-    }
-  },
-  button: {
-    maxWidth: 130,
-    marginTop: 50
-  }
-}))
-
 const ResponsePlug: React.FC<ResponsePlugProps> = ({ text, onClose }) => {
-  const classes = useStyles()
-
   return (
-    <div className={classes.root}>
-      <div className={classes.content}>
+    <Container>
+      <Content>
         <EmptyCartIcon />
         <p>{text}</p>
-      </div>
-      <Button
+      </Content>
+      <BackButton
         fullWidth
         onClick={onClose}
         darkLoader
         color="secondary"
-        startIcon={<ArrowBackIosIcon />}
-        className={classes.button}
+        startIcon={
+          <ExpandIcon>
+            <ArrowIcon />
+          </ExpandIcon>
+        }
       >
         Назад
-      </Button>
-    </div>
+      </BackButton>
+    </Container>
   )
 }
 
