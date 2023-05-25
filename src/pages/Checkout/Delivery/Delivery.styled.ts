20db0505b5de153dc5d34f88d0754f2af9ae6920
fresonn <vladislavs319@gmail.com>
2023-05-25 12:09:22 +0300

    checkout: delivery section based on "styled"

--- src/pages/Checkout/Delivery/Delivery.styled.ts
+++ src/pages/Checkout/Delivery/Delivery.styled.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components'
 import SvgIcon from '../../../shared/SvgIcon'
+import Button from '../../../shared/Button/Button'
+import { Field } from 'formik'
 
 export const Container = styled.section`
   background-color: ${({ theme }) => (theme.type === 'light' ? '#fff' : '#323232')};
@@ -75,5 +77,128 @@ export const DeliveriesItem = styled.li`
   @media ${({ theme }) => theme.media.xl} {
     flex-basis: 25%;
     margin: 0 10px;
+  }
+`
+
+export const ServiceLabel = styled.label`
+  display: block;
+`
+
+const hiddenStyles = css`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+`
+
+export const ImageWrapper = styled.div`
+  height: 83px;
+  cursor: pointer;
+  position: relative;
+  border-radius: 10px;
+  border: 1px solid #d7d7d7;
+  padding: 10px 10px;
+  transition: border-color 0.3s;
+  background-color: #fff;
+  &:hover {
+    border-color: var(--green-light);
+  }
+  &::before {
+    content: '';
+    position: absolute;
+    top: 7px;
+    left: 7px;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    border: 1px solid #aeaeae;
+    opacity: 0;
+    // transform: 'scale(0)', // TODO: bounce?
+    transition: 0.25s ease;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' fill='%23FFFFFF' viewBox='0 0 256 256'%3E%3Crect width='256' height='256' fill='none'%3E%3C/rect%3E%3Cpolyline points='216 72.005 104 184 48 128.005' fill='none' stroke='%23FFFFFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'%3E%3C/polyline%3E%3C/svg%3E");
+    background-size: 12px;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+  }
+`
+
+export const DeliveryInput = styled(Field)`
+  ${hiddenStyles}
+  &:checked + ${ImageWrapper} {
+    border-color: var(--green-light);
+    background-color: ${({ theme }) => (theme.type === 'light' ? 'rgba(50, 205, 50, .2)' : '#fff')};
+    &::before {
+      transform: scale(1);
+      opacity: 1;
+      background-color: var(--green-light);
+      border-color: var(--green-light);
+    }
+  }
+`
+
+export const EditPlug = styled.span<{ $hide: boolean }>`
+  display: ${({ $hide }) => ($hide ? 'none' : 'block')};
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 10px;
+  color: var(--green-light);
+  border-radius: 10px;
+  border: 2px solid var(--green-light);
+  padding: 5px 7px;
+  font-weight: 600;
+
+  @media ${({ theme }) => theme.media.lg} {
+    display: none;
+  }
+`
+
+export const AnimatedBox = styled.div`
+  padding: 0 10px;
+
+  @media ${({ theme }) => theme.media.lg} {
+    padding: 0 5px;
+    opacity: 1 !important;
+    height: auto !important;
+  }
+`
+
+export const FormField = styled.div`
+  width: 100%;
+  & > span {
+    font-size: 15px;
+    color: ${({ theme }) => (theme.type === 'light' ? '#6a6a6a' : '#fff')};
+    font-weight: 500;
+    padding-left: 7px;
+    margin-bottom: 5px;
+  }
+  @media screen and (min-width: 1000px) {
+    max-width: 300px;
+  }
+`
+
+export const AreaContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  @media ${({ theme }) => theme.media.xl} {
+    justify-content: flex-start;
+  }
+`
+
+export const ContinueButton = styled(Button)`
+  background-color: var(--green-light);
+  &:hover {
+    background-color: var(--green-light);
+  }
+
+  @media ${({ theme }) => theme.media.lg} {
+    display: none;
   }
 `
--- src/pages/Checkout/Delivery/Delivery.tsx
+++ src/pages/Checkout/Delivery/Delivery.tsx
@@ -1,137 +1,38 @@
 import React, { useState, useEffect } from 'react'
-import clsx from 'clsx'
-import FormControl from '@material-ui/core/FormControl'
 import TextInput from '../../../shared/FormFields/TextInput/TextInput'
 import ImagePlaceholder from '../../../shared/ImagePlaceholder'
 import novaPoshtaImage from '../../../assets/svg/nova_poshta.svg'
 import urkPoshtaImage from '../../../assets/svg/ukr-poshta.svg'
 import justinImage from '../../../assets/svg/justin.svg'
-import Button from '../../../shared/Button/Button'
 import SomethingWrongModal from '../Modals/SomethingWrong'
 import { CheckoutOrderType } from '../../../utils/validationSchema'
 import { ReactComponent as PinIcon } from '../../../assets/svg/icons/pin.svg'
-import { Field, useFormikContext } from 'formik'
-import { hiddenStyles } from '../../../utils/styling'
+import { useFormikContext } from 'formik'
 import { animated, useSpring } from 'react-spring'
-import { makeStyles } from '@material-ui/core'
 import Select from '../../../shared/FormFields/Select'
 
-import { Container, TitleWrapper, TitleBox, ThePinIcon, DeliveriesList, DeliveriesItem } from './Delivery.styled'
+import {
+  Container,
+  TitleWrapper,
+  TitleBox,
+  ThePinIcon,
+  DeliveriesList,
+  DeliveriesItem,
+  ServiceLabel,
+  DeliveryInput,
+  ImageWrapper,
+  EditPlug,
+  AnimatedBox,
+  AreaContainer,
+  FormField,
+  ContinueButton
+} from './Delivery.styled'
 
 interface DeliveryProps {
   isEdit: boolean
   onEdit(): void
   onContinue(): void
 }
-
-const useStyles = makeStyles((theme) => ({
-  serviceLabel: {
-    display: 'block'
-  },
-  input: {
-    ...hiddenStyles,
-    '&:checked + $imageWrapper': {
-      borderColor: 'var(--green-light)',
-      backgroundColor: theme.palette.type === 'light' ? 'rgba(50, 205, 50, .2)' : '#fff',
-      '&::before': {
-        transform: 'scale(1)',
-        opacity: 1,
-        backgroundColor: 'var(--green-light)',
-        borderColor: 'var(--green-light)'
-      }
-    }
-  },
-  imageWrapper: {
-    height: 83,
-    cursor: 'pointer',
-    position: 'relative',
-    borderRadius: 10,
-    border: '1px solid #d7d7d7',
-    padding: '10px 10px',
-    transition: 'border-color 0.3s',
-    backgroundColor: '#fff',
-    '&:hover': {
-      borderColor: 'var(--green-light)'
-    },
-    '&::before': {
-      content: "''",
-      position: 'absolute',
-      top: 7,
-      left: 7,
-      width: 15,
-      height: 15,
-      borderRadius: '50%',
-      border: '1px solid #aeaeae',
-      opacity: 0,
-      // transform: 'scale(0)', // TODO: bounce?
-      transition: '0.25s ease',
-      backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='192' height='192' fill='%23FFFFFF' viewBox='0 0 256 256'%3E%3Crect width='256' height='256' fill='none'%3E%3C/rect%3E%3Cpolyline points='216 72.005 104 184 48 128.005' fill='none' stroke='%23FFFFFF' stroke-linecap='round' stroke-linejoin='round' stroke-width='32'%3E%3C/polyline%3E%3C/svg%3E")`,
-      backgroundSize: '12px',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: '50% 50%'
-    }
-  },
-  editPlugHide: {
-    display: 'none'
-  },
-  editPlug: {
-    position: 'absolute',
-    left: '50%',
-    transform: 'translateX(-50%)',
-    bottom: 10,
-    color: 'var(--green-light)',
-    borderRadius: 10,
-    border: '2px solid var(--green-light)',
-    padding: '5px 7px',
-    fontWeight: 600,
-    [theme.breakpoints.up('lg')]: {
-      display: 'none'
-    }
-  },
-  animatedBox: {
-    padding: '0 10px',
-    [theme.breakpoints.up('lg')]: {
-      opacity: '1 !important',
-      height: 'auto !important'
-    }
-  },
-  formField: {
-    width: '100%',
-    '& > span': {
-      fontSize: 15,
-      color: theme.palette.type === 'light' ? '6a6a6a' : '#fff',
-      fontWeight: 500,
-      paddingLeft: 7,
-      marginBottom: 5
-    },
-    ['@media screen and (min-width: 1000px)']: {
-      maxWidth: 300
-    }
-  },
-  areaContainer: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    [theme.breakpoints.up('xl')]: {
-      justifyContent: 'flex-start'
-    }
-  },
-  cityField: {
-    [theme.breakpoints.up('xl')]: {
-      width: 230,
-      marginLeft: 25
-    }
-  },
-  сontinueButton: {
-    backgroundColor: 'var(--green-light)',
-    '&:hover': {
-      backgroundColor: 'var(--green-light)'
-    },
-    [theme.breakpoints.up('lg')]: {
-      display: 'none'
-    }
-  }
-}))
 
 const API_URL = process.env.REACT_APP_API_URL
 
@@ -146,8 +47,6 @@ interface AreasType {
 }
 
 const Delivery: React.FC<DeliveryProps> = ({ isEdit, onEdit, onContinue }) => {
-  const classes = useStyles()
-
   const { values } = useFormikContext<CheckoutOrderType>()
 
   const [areas, setAreas] = useState<AreasType>()
@@ -205,53 +104,53 @@ const Delivery: React.FC<DeliveryProps> = ({ isEdit, onEdit, onContinue }) => {
           </ThePinIcon>
           <h2>Способ доставки</h2>
         </TitleBox>
-        <span className={clsx(isEdit ? classes.editPlugHide : classes.editPlug)}>Изменить</span>
+        <EditPlug $hide={isEdit}>Изменить</EditPlug>
       </TitleWrapper>
-      <animated.div style={{ ...slideInStyles, overflow: 'hidden' }} className={classes.animatedBox}>
+      <AnimatedBox as={animated.div} style={{ ...slideInStyles, overflow: 'hidden' }}>
         <DeliveriesList>
           <DeliveriesItem>
-            <label className={classes.serviceLabel}>
-              <Field type="radio" name="supplier" value="nova-poshta" className={classes.input} />
-              <div className={classes.imageWrapper}>
+            <ServiceLabel>
+              <DeliveryInput type="radio" name="supplier" value="nova-poshta" />
+              <ImageWrapper>
                 <ImagePlaceholder plain src={novaPoshtaImage} altText="логотип 'Новая Почта'" />
-              </div>
-            </label>
+              </ImageWrapper>
+            </ServiceLabel>
           </DeliveriesItem>
           <DeliveriesItem>
-            <label className={classes.serviceLabel}>
-              <Field type="radio" name="supplier" value="url-poshta" className={classes.input} />
-              <div className={classes.imageWrapper}>
+            <ServiceLabel>
+              <DeliveryInput type="radio" name="supplier" value="url-poshta" />
+              <ImageWrapper>
                 <ImagePlaceholder plain src={urkPoshtaImage} altText="логотип 'Укр Почта'" />
-              </div>
-            </label>
+              </ImageWrapper>
+            </ServiceLabel>
           </DeliveriesItem>
           <DeliveriesItem>
-            <label className={classes.serviceLabel}>
-              <Field type="radio" name="supplier" value="justin" className={classes.input} />
-              <div className={classes.imageWrapper}>
+            <ServiceLabel>
+              <DeliveryInput type="radio" name="supplier" value="justin" />
+              <ImageWrapper>
                 <ImagePlaceholder plain src={justinImage} altText="логотип 'Justin'" />
-              </div>
-            </label>
+              </ImageWrapper>
+            </ServiceLabel>
           </DeliveriesItem>
         </DeliveriesList>
-        <div className={classes.areaContainer}>
-          <FormControl className={classes.formField}>
+        <AreaContainer>
+          <FormField>
             <span>Область</span>
             <Select name="region" disabled={areasLoading} options={areasOptions} />
-          </FormControl>
-          <FormControl className={clsx(classes.formField, classes.cityField)}>
+          </FormField>
+          <FormField>
             <span>Город</span>
             <Select name="cityId" disabled={!values.region} options={citiesOptions} />
-          </FormControl>
-        </div>
-        <FormControl className={classes.formField}>
+          </FormField>
+        </AreaContainer>
+        <FormField>
           <span>Выберите отделение</span>
           <TextInput name="postOfficeId" />
-        </FormControl>
-        <Button disableShadow fullWidth onClick={onContinue} className={classes.сontinueButton}>
+        </FormField>
+        <ContinueButton disableShadow fullWidth onClick={onContinue}>
           Продолжить
-        </Button>
-      </animated.div>
+        </ContinueButton>
+      </AnimatedBox>
       <SomethingWrongModal open={areasError} />
     </Container>
   )
