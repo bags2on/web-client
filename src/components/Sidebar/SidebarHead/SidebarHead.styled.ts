95e1bdf784887e8f999c561c5f5220714c3f514e
fresonn <vladislavs319@gmail.com>
2023-06-21 20:07:47 +0300

    sidebar: header styles update

--- src/components/Sidebar/SidebarHead/SidebarHead.styled.ts
+++ src/components/Sidebar/SidebarHead/SidebarHead.styled.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   position: relative;
-  padding: 60px 0 25px 20px;
+  padding: 40px 0 30px 20px;
+  background-color: #1e1e1e;
   border-bottom: 1px #aeaeae solid;
 `
 
 export const LogoWrapper = styled.div`
   width: 190px;
-  background-color: ${({ theme }) => (theme.type === 'light' ? 'transparent' : theme.colors.primary)};
   & > img {
     width: 100%;
     height: 100%;
