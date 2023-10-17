725fa852a8d4c95a5b8494da18f677b0fdf31dc1
fresonn <vladislavs319@gmail.com>
2023-10-17 18:15:35 +0300

    cart item with "skeleton"

--- src/components/Cart/CartItems/ListSkeleton/index.tsx
+++ src/components/Cart/CartItems/ListSkeleton/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ContentLoader from 'react-content-loader'
+import CartItemSkeleton from '../../CartItem/Skeleton'
 import styled from 'styled-components'
 
 const List = styled.ul`
@@ -25,20 +25,7 @@ const ListSkeleton: React.FC<ListSkeletonProps> = ({ itemsAmount }) => {
   return (
     <List>
       {[...Array(itemsLength)].map((_, index: number) => (
-        <li key={index}>
-          <ContentLoader
-            backgroundColor="#eeeeee"
-            foregroundColor="#e1e1e1"
-            width="100%"
-            height="171"
-            viewBox="0 0 400 171"
-          >
-            <rect x="20" y="5" rx="6" ry="6" width="130" height="155" />
-            <rect x="190" y="25" rx="6" ry="6" width="190" height="21" />
-            <rect x="190" y="63" rx="6" ry="6" width="130" height="20" />
-            <rect x="190" y="94" rx="6" ry="6" width="85" height="20" />
-          </ContentLoader>
-        </li>
+        <CartItemSkeleton key={index} />
       ))}
     </List>
   )
