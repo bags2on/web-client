a4ea9fa63edf20ffaae52e1b49aae65a6df2142e
fresonn <vladislavs319@gmail.com>
2023-11-08 14:55:05 +0200

    api: new "getWarehouses" route

---
+++ src/pages/api/getWarehouses.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type RequestData = {
+  city_id: string
+  provider: string
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { city_id, provider }: RequestData = JSON.parse(req.body)
+
+    if (!city_id || !provider) {
+      res.status(500).json({ message: 'city_id or provider are not specified' })
+      return
+    }
+
+    const params = new URLSearchParams({
+      city_id,
+      provider
+    })
+
+    const remoteData = await fetch(process.env.NEXT_PUBLIC_DELIVERY_HOST + '/warehouses?' + params)
+    const rawBytes = await remoteData.arrayBuffer()
+
+    res.status(200).send(Buffer.from(rawBytes))
+  } catch (error) {
+    res.status(500).json({ message: 'warehouses service not respond' })
+  }
+}
