6e555930da8ef7a798db5abfa824c2dd95975875
fresonn <vladislavs319@gmail.com>
2023-11-08 15:00:54 +0200

    api: "getPopularCities" without unnecessary data processing

--- src/pages/api/getPopularCities.ts
+++ src/pages/api/getPopularCities.ts
@@ -5,11 +5,11 @@ type Data = {
   error: string
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | Buffer>) {
   try {
     const remoteData = await fetch(process.env.NEXT_PUBLIC_DELIVERY_HOST + '/popular-cities')
-    const jsonData = await remoteData.json()
-    res.status(200).json(jsonData)
+    const rawBytes = await remoteData.arrayBuffer()
+    res.status(200).send(Buffer.from(rawBytes))
   } catch (error) {
     res.status(500).json({ error: 'delivery service not respond' })
   }
