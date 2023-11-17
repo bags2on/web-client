658762bf5cd3d32554f7952d0abc347399e7e85e
fresonn <vladislavs319@gmail.com>
2023-11-17 19:02:37 +0200

    fixed warning in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-
 const path = require('path')
 const { i18n } = require('./next-i18next.config')
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   webpack(config) {
