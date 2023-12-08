791a0b8ca1bc259d0199d0237c536fe2c7df5679
fresonn <vladislavs319@gmail.com>
2023-12-08 16:50:07 +0200

    main product: styled with CSS

--- src/components/MainProduct/MainProduct.tsx
+++ src/components/MainProduct/MainProduct.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
 import { routeNames, generateProductLink } from '@/utils/navigation'
 
-import {
-  Container,
-  AnimatedBackground,
-  InnerBox,
-  Image,
-  PriceBox,
-  Price,
-  Title
-} from './MainProduct.styled'
+import styles from './MainProduct.module.scss'
 
 interface MainProductProps {
   id: string
@@ -19,24 +12,28 @@ interface MainProductProps {
 
 const MainProduct: React.FC<MainProductProps> = ({ id, title, price }) => {
   return (
-    <Container>
-      <AnimatedBackground />
-      <InnerBox href={generateProductLink(routeNames.product, id, 'TODO')}>
-        <Image
+    <section className={styles.container}>
+      <div className={styles.background} />
+      <Link
+        href={generateProductLink(routeNames.product, id, 'TODO')}
+        className={styles.linkWrapper}
+      >
+        <img
+          className={styles.image}
           src={
             'https://res.cloudinary.com/dct4oinuz/image/upload/v1683563023/bags2on/prada-black-leather_asdgjb.png'
           }
           alt="самый рекомендуемый товар"
         />
-        <PriceBox>
-          <Price>
+        <div className={styles.priceBox}>
+          <p className={styles.price}>
             {price}
             <span>грн.</span>
-          </Price>
-        </PriceBox>
-        {title && <Title>{title}</Title>}
-      </InnerBox>
-    </Container>
+          </p>
+        </div>
+        {title && <p className={styles.title}>{title}</p>}
+      </Link>
+    </section>
   )
 }
 
