b9e3c48bb3acaeb9c28206d1f93d4189fbf9f22b
fresonn <vladislavs319@gmail.com>
2023-12-13 16:05:18 +0200

    cart: top controls styled with CSS

--- src/components/Cart/TopControls/TopControls.tsx
+++ src/components/Cart/TopControls/TopControls.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
+import clsx from 'clsx'
+import Button from '@/shared/Button'
+import IconButton from '@/shared/IconButton'
 import CrossIcon from '../../../../public/assets/icons/cross.svg'
 import TrashIcon from '../../../../public/assets/icons/trash.svg'
 import { useTranslation } from 'next-i18next'
 import { CartMutations } from '@/apollo/cache/mutations'
 
-import {
-  Container,
-  ClearButton,
-  CloseButton,
-  TheTrashIcon,
-  TheCloseIcon,
-  InnerContainer,
-  Title
-} from './TopControls.styled'
-
+import styles from './TopControls.module.scss'
 interface TopControlsProps {
   onCartClose(): void
 }
@@ -26,29 +20,30 @@ const TopControls: React.FC<TopControlsProps> = ({ onCartClose }) => {
   }
 
   return (
-    <Container>
-      <InnerContainer>
-        <CloseButton disableRipple onClick={onCartClose}>
-          <TheCloseIcon>
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <IconButton disableRipple onClick={onCartClose} className={styles.closeButton}>
+          <div className={clsx('svg-icon', styles.closeIcon)}>
             <CrossIcon />
-          </TheCloseIcon>
-        </CloseButton>
-        <Title>
+          </div>
+        </IconButton>
+        <p className={styles.title}>
           <b>{t('cart.topControls.title1')}</b>&nbsp;{t('cart.topControls.title2')}
-        </Title>
-        <ClearButton
+        </p>
+        <Button
           color="secondary"
           onClick={handleClearButtonClick}
+          className={styles.clearButton}
           startIcon={
-            <TheTrashIcon>
+            <div className={clsx('svg-icon', styles.trashIcon)}>
               <TrashIcon />
-            </TheTrashIcon>
+            </div>
           }
         >
           {t('cart.topControls.action')}
-        </ClearButton>
-      </InnerContainer>
-    </Container>
+        </Button>
+      </div>
+    </div>
   )
 }
 
