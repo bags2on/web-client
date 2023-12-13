cc84ebe252be61c3fa2ab5c2b423658e9d4186d1
fresonn <vladislavs319@gmail.com>
2023-12-13 17:13:08 +0200

    cart: summary styled with CSS

--- src/components/Cart/Summary/Skeleton.tsx
+++ src/components/Cart/Summary/Skeleton.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ContentLoader from 'react-content-loader'
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  padding: 15px 10px 19px 15px;
-`
+import styles from './Summary.module.scss'
 
 const Skeleton = () => {
   return (
-    <Wrapper>
+    <div className={styles.skeleton}>
       <ContentLoader
         backgroundColor="#eeeeee"
         foregroundColor="#e1e1e1"
@@ -20,7 +16,7 @@ const Skeleton = () => {
         <rect x="74%" y="0" rx="6" ry="6" width="100" height="28" />
         <rect x="0" y="35" rx="14" ry="14" width="100%" height="50" />
       </ContentLoader>
-    </Wrapper>
+    </div>
   )
 }
 
