53071d1c3f368cba85505c0ab77c43528185acc3
fresonn <vladislavs319@gmail.com>
2023-12-15 19:23:14 +0200

    link badge: works as "wrapper" now

--- src/shared/LinkBadge/index.tsx
+++ src/shared/LinkBadge/index.tsx
@@ -5,28 +5,28 @@ import styles from './LinkBadge.module.scss'
 interface LinkBadgeProps {
   top?: number
   right?: number
+  children: React.ReactNode
 }
 
-// TODO: delete after MIGRATION
-// make as wrapper with iconClassname animation
-export const iconClassname = styles.icon
-
-const LinkBadge: React.FC<LinkBadgeProps> = ({ top = 10, right = 13 }) => {
+const LinkBadge: React.FC<LinkBadgeProps> = ({ top = 10, right = 13, children }) => {
   return (
-    <div
-      className={styles.mark}
-      style={{
-        top,
-        right
-      }}
-    >
-      <Image
-        width={25}
-        height={25}
-        src="/assets/icons/expand-arrow.svg"
-        alt="TODO"
-        className={styles.icon}
-      />
+    <div className={styles.container}>
+      <div
+        className={styles.mark}
+        style={{
+          top,
+          right
+        }}
+      >
+        <Image
+          width={25}
+          height={25}
+          src="/assets/icons/expand-arrow.svg"
+          alt="icon"
+          className={styles.icon}
+        />
+      </div>
+      {children}
     </div>
   )
 }
