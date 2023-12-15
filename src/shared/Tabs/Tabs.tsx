995be3b1d54f46d8f2bbfe713704e874a36867d4
fresonn <vladislavs319@gmail.com>
2023-12-15 19:40:25 +0200

    shared: tabs styled with CSS

--- src/shared/Tabs/Tabs.tsx
+++ src/shared/Tabs/Tabs.tsx
@@ -1,52 +1,7 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
+import clsx from 'clsx'
 
-const TabList = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-
-type tabStyledProps = {
-  $active: boolean
-  $disabled: boolean
-}
-
-const Tab = styled.li<tabStyledProps>`
-  cursor: pointer;
-  font-size: 14px;
-  border: 2px solid #eaeaea;
-  padding: 6px 12px;
-  text-align: center;
-  font-weight: 600;
-  margin-right: 8px;
-  border-radius: 8px;
-  user-select: none;
-  transition: all 300ms;
-
-  &:hover {
-    border-color: #232323;
-  }
-
-  ${({ $active }) =>
-    $active &&
-    css`
-      color: #fff;
-      border-color: #232323;
-      background-color: #232323;
-    `}
-
-  ${({ $disabled }) =>
-    $disabled &&
-    css`
-      color: #7c7c7c;
-      border-color: #f0f0f0;
-      background-color: #f0f0f0;
-      &:hover {
-        border-color: #f0f0f0;
-      }
-    `}
-`
+import styles from './Tabs.module.scss'
 
 interface TabsPorps {
   activeTab: number
@@ -65,18 +20,21 @@ const Tabs: React.FC<TabsPorps> = ({ activeTab, tabs, onChange }) => {
   }
 
   return (
-    <TabList>
+    <ul className={styles.tabList}>
       {tabs.map(({ label, disabled = false }, ind) => (
-        <Tab
+        <li
           key={label}
-          $active={activeTab === ind}
-          $disabled={disabled}
           onClick={() => handleTabClick(ind, disabled)}
+          className={clsx({
+            [styles.tab]: true,
+            [styles.active]: activeTab === ind,
+            [styles.disabled]: disabled
+          })}
         >
           {label}
-        </Tab>
+        </li>
       ))}
-    </TabList>
+    </ul>
   )
 }
 
