c988853b6b64d3d5d9ab0da743bd30be6624b360
fresonn <vladislavs319@gmail.com>
2023-12-17 21:32:52 +0200

    shared: page loader styled with CSS

--- src/shared/PageLoader/index.tsx
+++ src/shared/PageLoader/index.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
-
 import ScaleLoader from '@/shared/loaders/ScaleLoader'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
 
 const PageLoader = () => {
   return (
-    <Container>
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
       <ScaleLoader fallback />
-    </Container>
+    </div>
   )
 }
 
