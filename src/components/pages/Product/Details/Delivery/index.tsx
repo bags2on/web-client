1b0589d1b077cc83aa686efceb26c479877b00f0
fresonn <vladislavs319@gmail.com>
2023-12-17 15:25:23 +0200

    product: delivery section styled with CSS

--- src/components/pages/Product/Details/Delivery/index.tsx
+++ src/components/pages/Product/Details/Delivery/index.tsx
@@ -1,50 +1,8 @@
 import React from 'react'
+import clsx from 'clsx'
 import Image from 'next/image'
-import styled, { css } from 'styled-components'
 
-const DeliveryList = styled.ul`
-  margin-top: 28px;
-  list-style: none;
-  padding: 0;
-`
-
-const DeliveryItem = styled.li`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-size: 14px;
-  line-height: 15px;
-  font-weight: 500;
-  margin-bottom: 13px;
-  @media ${({ theme }) => theme.media.lg} {
-    font-size: 15px;
-  }
-  & p {
-    margin: 0;
-    text-align: right;
-  }
-`
-
-const Info = styled.div`
-  display: flex;
-  align-items: center;
-
-  & p {
-    margin-left: 7px;
-    text-align: left;
-  }
-`
-
-const Description = styled.p<{ $free: boolean }>`
-  ${({ $free }) =>
-    $free &&
-    css`
-      color: #343434;
-      background-color: #ffff83;
-      border-radius: 12px;
-      padding: 4px 6px;
-    `}
-`
+import styles from './Delivery.module.scss'
 
 interface DeliveryProps {
   free: boolean
@@ -52,9 +10,9 @@ interface DeliveryProps {
 
 const Delivery: React.FC<DeliveryProps> = ({ free }) => {
   return (
-    <DeliveryList>
-      <DeliveryItem>
-        <Info>
+    <ul className={styles.container}>
+      <li className={styles.deliveryItem}>
+        <div className={styles.info}>
           <Image
             src="/assets/icons/novaposta.svg"
             width={30}
@@ -62,11 +20,11 @@ const Delivery: React.FC<DeliveryProps> = ({ free }) => {
             alt={'В отделение «Нова пошта»'}
           />
           <p>В отделение «Нова пошта»</p>
-        </Info>
-        <Description $free={free}>{free ? 'Бесплатно' : 'от 100 ₴'}</Description>
-      </DeliveryItem>
-      <DeliveryItem>
-        <Info>
+        </div>
+        <p className={clsx(free && styles.freeDelivery)}>{free ? 'Бесплатно' : 'от 100 ₴'}</p>
+      </li>
+      <li className={styles.deliveryItem}>
+        <div className={styles.info}>
           <Image
             src="/assets/icons/novaposta.svg"
             width={30}
@@ -74,11 +32,11 @@ const Delivery: React.FC<DeliveryProps> = ({ free }) => {
             alt={'Курьер Новой Почты по вашему адресу'}
           />
           <p>Курьер по вашему адресу</p>
-        </Info>
+        </div>
         <p>по тарифам перевозчика</p>
-      </DeliveryItem>
-      <DeliveryItem>
-        <Info>
+      </li>
+      <li className={styles.deliveryItem}>
+        <div className={styles.info}>
           <Image
             src="/assets/icons/ukrposta.svg"
             width={31}
@@ -86,10 +44,12 @@ const Delivery: React.FC<DeliveryProps> = ({ free }) => {
             alt={'В отделение «Укрпошта»'}
           />
           <p>В отделение «Укрпошта»</p>
-        </Info>
-        <Description $free={free}>{free ? 'Бесплатно' : 'по тарифам перевозчика'}</Description>
-      </DeliveryItem>
-    </DeliveryList>
+        </div>
+        <p className={clsx(free && styles.freeDelivery)}>
+          {free ? 'Бесплатно' : 'по тарифам перевозчика'}
+        </p>
+      </li>
+    </ul>
   )
 }
 
